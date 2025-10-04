3b2393e58ae7fee37ae46fc2f6c573eb04e02ccd
Hariharan C <hariharandeveloper05@gmail.com>
2025-10-04 15:37:26 +0530

    Base URL Updated

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://snipr-server-production.up.railway.app/",
   withCredentials: true,
 });
 
